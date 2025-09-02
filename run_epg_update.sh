56e2b86df00f9664a09735daefdef0486021a557
Steven Coutts <stevec@couttsnet.com>
2025-09-02 13:03:08 +0100

    chore: remove EPG_SOURCE presence check

--- run_epg_update.sh
+++ run_epg_update.sh
@@ -11,10 +11,6 @@ if [ -f "$SCRIPT_DIR/.env" ]; then
 fi
 
 URL="${EPG_SOURCE:-}"
-if [ -z "$URL" ]; then
-	echo "EPG_SOURCE is not set. Create $SCRIPT_DIR/.env with: EPG_SOURCE=\"https://www.open-epg.com/app/pdownload.php?file=ksy5SdW4jS.xml\"" >&2
-	exit 1
-fi
 OUTPUT="open-epg_series.xml"
 
 exec python3 "$SCRIPT_DIR/epg_add_series.py" --url "$URL" --output "$OUTPUT"
